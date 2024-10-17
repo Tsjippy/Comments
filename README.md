da11836d9386a85748313991816c29c461e3a17d
Tsjippy <ewaldha@hotmail.com>
2024-10-17 09:55:24 +0100

    changed: readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-A module for the sim-plugin to add signal messaging
+This module allows you to define the e-mails send when someone adds a comment to the website.<br>
+You can also define on which post types comments are allowed<br>
+You can turn on or off comments also on a per page level.
--- php/__module_menu.php
+++ php/__module_menu.php
@@ -9,24 +9,6 @@ DEFINE(__NAMESPACE__.'\MODULE_PATH', plugin_dir_path(__DIR__));
 //module slug is the same as grandparent folder name
 DEFINE(__NAMESPACE__.'\MODULE_SLUG', strtolower(basename(dirname(__DIR__))));
 
-add_filter('sim_submenu_description', function($description, $moduleSlug){
-	//module slug should be the same as the constant
-	if($moduleSlug != MODULE_SLUG)	{
-		return $description;
-	}
-
-	ob_start();
-	?>
-	<p>
-		This module allows you to define the e-mails send when someone adds a comment to the website.<br>
-		You can also define on which post types comments are allowed<br>
-		You can turn on or off comments also on a per page level.
-	</p>
-	<?php
-
-	return ob_get_clean();
-}, 10, 2);
-
 add_filter('sim_submenu_options', function($optionsHtml, $moduleSlug, $settings){
 	//module slug should be the same as grandparent folder name
 	if($moduleSlug != MODULE_SLUG){
